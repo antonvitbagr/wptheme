100123c1d5ca3f82de5d81f27c0d7e7e616569cf
Anton <antonvitbagr@mail.ru>
2017-08-26 09:51:29 +0800

    more files

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+  /**
+   * Define names
+   *
+   *
+   *
+   */
+  
+  $theme_name = "sandbox";
+
+  /**
+   * Define paths of the theme
+   *
+   *
+   *
+   *  
+   */
+  define("THEME_PATH", WP_CONTENT_URL."/themes/".$theme_name."/"); 
---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="<?php echo THEME_PATH; ?>style.css">
+</head>
+<body>
+
